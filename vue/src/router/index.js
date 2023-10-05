05c432c039217481c03d1678bc5d82a1144dd57b
JC Monsieur Edmund Tubiera <monsieurphenomenal@gmail.com>
2023-10-05 20:49:46 +0800

    #9 frontend authentication

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import store from "../store";
 
 const routes = [
     {
@@ -13,6 +14,7 @@ const routes = [
         path: "/dashboard",
         name: "dashboard",
         component: Dashboard,
+        meta: { requiresAuth: true },
     },
     {
         path: "/login",
@@ -24,6 +26,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "login" });
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({ name: "dashboard" });
+    } else {
+        next();
+    }
 });
 
 export default router;
